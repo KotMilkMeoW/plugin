196c49b724fc71bd8eb64e288910cbf97eace79d
KotMilkMeoW <81019712+KotMilkMeoW@users.noreply.github.com>
2021-08-23 20:49:57 +0300

    Update Main.java

--- src/main/java/meowland/Main.java
+++ src/main/java/meowland/Main.java
@@ -1,1 +1,12 @@
+package meowland;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin
+{
+    @Override
+    public void onEnable()
+    {
+        this.getLogger().info("ПЛУГИН ЗАПУСТИЛСЯ!!! (УДИВИТЕЛЬНО)");
+    }
+}
