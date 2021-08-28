a47418d962bf60d5354d04467a6fc5c1319c9ba0
KotMilkMeoW <81019712+KotMilkMeoW@users.noreply.github.com>
2021-08-28 22:21:51 +0300

    Update Main.java

--- src/main/java/meowland/Main.java
+++ src/main/java/meowland/Main.java
@@ -1,12 +1,12 @@
 package meowland;
 
+import prg.bukkit.*;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.player.PlayerListener;
 import org.bukkit.Server;
-import org.bukkit.command;
-import org.bukkit.event;
+import org.bukkit.command.Command;
 
 public class Main extends JavaPlugin
 {
