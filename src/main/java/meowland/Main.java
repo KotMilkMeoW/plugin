b417906be9bac03632d28a6701e47b8182629071
KotMilkMeoW <81019712+KotMilkMeoW@users.noreply.github.com>
2021-08-28 21:41:36 +0300

    Add first command

--- src/main/java/meowland/Main.java
+++ src/main/java/meowland/Main.java
@@ -9,4 +9,10 @@ public class Main extends JavaPlugin
     {
         this.getLogger().info("ПЛУГИН ЗАПУСТИЛСЯ!!! (УДИВИТЕЛЬНО)");
     }
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
+    if(cmd.getName().equalsIgnoreCase("Hi!")){
+         player.sendMessage("Hello!");
+    }
+    }
 }
