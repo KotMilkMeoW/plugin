637c4f4478f22a5ae22286c2afe14067308ceeca
KotMilkMeoW <81019712+KotMilkMeoW@users.noreply.github.com>
2021-08-29 21:54:59 +0300

    + импорты

--- src/main/java/meowland/Main.java
+++ src/main/java/meowland/Main.java
@@ -1,6 +1,8 @@
 package meowland;
 
 import org.bukkit.*;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
