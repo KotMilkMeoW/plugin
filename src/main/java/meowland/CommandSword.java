55548b8bd399fe3dc88d2433fd9aada411876f97
KotMilkMeoW <81019712+KotMilkMeoW@users.noreply.github.com>
2021-08-30 20:02:10 +0300

    Update CommandSword.java

--- src/main/java/meowland/CommandSword.java
+++ src/main/java/meowland/CommandSword.java
@@ -4,6 +4,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.enchantments.Enchantment;
@@ -14,9 +15,10 @@ public class CommandSword implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
          if(cmd.getName().equalsIgnoreCase("sword")){
              Player player = (Player) sender;
+             String xz = event.getName();
              ItemStack stack = new ItemStack(Material.DIAMOND_SWORD, 1);
              stack.addEnchantment(Enchantment.KNOCKBACK, 10);
-             sender.getInventory().addItem(stack);
+             xz.getInventory().addItem(stack);
              return true;
          }
          return false;
