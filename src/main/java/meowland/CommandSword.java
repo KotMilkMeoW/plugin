bb4785fa295788b015df24e19b1bbaad98b1c432
KotMilkMeoW <81019712+KotMilkMeoW@users.noreply.github.com>
2021-08-30 20:04:55 +0300

    Update CommandSword.java

--- src/main/java/meowland/CommandSword.java
+++ src/main/java/meowland/CommandSword.java
@@ -4,7 +4,6 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.event.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.enchantments.Enchantment;
