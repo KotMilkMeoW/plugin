a113a33356a2d4459d263f1e90311c75857750c4
KotMilkMeoW <81019712+KotMilkMeoW@users.noreply.github.com>
2021-09-01 18:33:59 +0300

    Create TpaCommand.java

---
+++ src/main/java/meowland/TpaCommand.java
@@ -0,0 +1,25 @@
+package meowland;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.event.Event;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.Material;
+
+public class CommandSword implements CommandExecutor {
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
+         if(cmd.getName().equalsIgnoreCase("tpa")){
+             //Потом сделаю
+             return true;
+         }
+         return false;
+}
+
+}
